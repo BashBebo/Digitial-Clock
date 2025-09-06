bb0f86ee7ae14d4f77145f079da82cdaa5ec728b
Bashir <bashir6412@gmail.com>
2025-09-06 10:24:18 +0400

    Nothing is woorking

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-// Digital Clock Script
-// Here in let and const are used to make the varabiles real and store the data in const
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
 let timer = null;
 let isRunning = false;
 
-// these are getting the elements from the ids and creating a variable for them
 const clockElement = document.getElementById('clock');
-const startStopBtn = document.getElementById('StartStopBtn');
+const startButton = document.getElementById('startStopBtn');
 
-
-// This function is used to format the time in two digits
 function formatNumber(num) {
-    return num < 10 ? "0" + num : num;
+    return num < 10 ? '0' + num : num;
 }
-// This function is used to update the clock
+
 function updateClock() {
     seconds++;
     if (seconds >= 60) {
         seconds = 0;
         minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+            if (hours >= 24) {
+                hours = 0;
+            }
+        }
     }
-    if (minutes >= 60) {
-        minutes = 0;
-        hours++;
-    }
-    if (hours >= 24) {
-        hours = 0;
-    }
-    clockElement.innerText =
-      `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;
+    clockElement.innerText = `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;
 }
 
-// handle start/Stop button click
-startStopBtn.addEventListener('click', () => {
-    if (!isRunning) {  // ❌ use !isRunning to start
-        timer = setInterval(updateClock, 1000); 
+startButton.addEventListener("click", () => {
+    if (!isRunning) {
+        // Start the clock
+        timer = setInterval(updateClock, 1000);
+        startButton.innerText = "Pause";
         isRunning = true;
-        startStopBtn.innerText = "Stop";
     } else {
+        // Stop the clock
         clearInterval(timer);
+        startButton.innerText = "Start";
         isRunning = false;
-        startStopBtn.innerText = "Start";
     }
 });
